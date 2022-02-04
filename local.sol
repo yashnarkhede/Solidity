d573653dde4b6eb7de362cce69ee24ab01b37735
Yash Narkhede <53682155+yashnarkhede@users.noreply.github.com>
2022-02-04 11:20:36 +0530

    Create local.sol

---
+++ local.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract local{
+    function store() pure public returns(uint){
+        string memory name="Yash";
+        uint age=10;
+        return age;
+
+    }
+
+}
