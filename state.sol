b5d766e6c4514dfc93258611c586b0f32a78a43d
Yash Narkhede <53682155+yashnarkhede@users.noreply.github.com>
2022-02-04 11:08:40 +0530

    Create state.sol

---
+++ state.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract state{
+    uint public age=5;
+
+    // constructor() public{
+    //     age=10;
+    // }
+    
+    // function setAge() public{
+    //     age=10;
+    // }
+}
