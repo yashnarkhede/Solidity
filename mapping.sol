e4f4199553a2d3b920fd04eb3465f4c712e244f0
Yash Narkhede <53682155+yashnarkhede@users.noreply.github.com>
2022-02-05 23:33:10 +0530

    Update mapping.sol

--- mapping.sol
+++ mapping.sol
@@ -1,1 +1,13 @@
+//SPDX-License-Identifier: GPL-3.0
 
+pragma solidity >=0.7.0 <0.9.0;
+
+contract demo{
+
+    mapping(uint=>string) public roll_no;
+    
+    function setter(uint keys, string memory value) public{
+        roll_no[keys]= value;
+    }
+
+}
